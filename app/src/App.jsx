7b455aa2be613e06be2e665926d70f5d0fb274bd
fernando-izar <fizar@me.com>
2026-05-11 10:15:01 -0300

    fix: clear old code

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,12 +1,11 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import HomePage from "./pages/HomePage";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import AssetDashboard from "./pages/AssetDashboard";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route path="/" element={<Navigate to="/dashboard" replace />} />
         <Route path="/dashboard" element={<AssetDashboard />} />
       </Routes>
     </BrowserRouter>
